16b84f33952b981cb8745a2465c3b13257679121
ClaytonalvesSs <clayton.santos63@etec.sp.gov.br>
2023-08-13 23:26:14 -0300

    Finalizando design do app

---
+++ app/src/main/java/com/example/app_exercicios/Activity_ex4.java
@@ -0,0 +1,14 @@
+package com.example.app_exercicios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_ex4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex4);
+    }
+}
