ea218bf80c1681da672bbec4ec5e74f0de6db69f
evelynkarina44 <evelyn.goncalves12@etec.sp.gov.br>
2023-08-15 17:07:49 -0300

    linkando as activitys

--- app/src/main/java/com/example/app_exercicios/MainActivity.java
+++ app/src/main/java/com/example/app_exercicios/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.app_exercicios;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void Exercicio1 (View v){
+        Intent exercicio1 = new Intent(MainActivity.this, Activity_ex1.class);
+        startActivity(exercicio1);
+    }
+    public void Exercicio4 (View v){
+        Intent exercicio4 = new Intent(MainActivity.this, Activity_ex4.class);
+        startActivity(exercicio4);
+    }
 }
+
