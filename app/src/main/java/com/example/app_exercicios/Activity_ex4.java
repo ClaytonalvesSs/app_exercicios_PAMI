1f009b3d686c1880f1c938efd5f6421732866ffe
evelynkarina44 <evelyn.goncalves12@etec.sp.gov.br>
2023-08-18 17:37:43 -0300

    linkando as activitys

--- app/src/main/java/com/example/app_exercicios/Activity_ex4.java
+++ app/src/main/java/com/example/app_exercicios/Activity_ex4.java
@@ -3,12 +3,28 @@ package com.example.app_exercicios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Activity_ex4 extends AppCompatActivity {
+    private EditText ET_hrentrada;
+    private EditText ET_hrsaida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex4);
+        ET_hrentrada = findViewById(R.id.ET_hrentrada);
+        ET_hrsaida = findViewById(R.id.ET_hrsaida);
+    }
+    public void horaCalculo(View Vi){
+        int hrentrada = Integer.parseInt(ET_hrentrada.getText().toString());
+
+        int hrsaida = hrentrada + 6;
+
+        if (hrsaida >= 24) {
+            hrsaida -= 24;
+        }
+        ET_hrsaida.setText(hrsaida);
     }
 }
