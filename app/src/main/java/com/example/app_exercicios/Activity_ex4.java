7cef98cf1559e9ca877bd0c684ff5684f796914c
ClaytonalvesSs <clayton.santos63@etec.sp.gov.br>
2023-08-19 00:17:04 -0300

    finalizando exercicio 4

--- app/src/main/java/com/example/app_exercicios/Activity_ex4.java
+++ app/src/main/java/com/example/app_exercicios/Activity_ex4.java
@@ -2,21 +2,25 @@ package com.example.app_exercicios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity_ex4 extends AppCompatActivity {
         private EditText ET_hrentrada;
-        private EditText ET_hrsaida;
+        private TextView txt_saida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex4);
         ET_hrentrada = findViewById(R.id.ET_hrentrada);
-        ET_hrsaida = findViewById(R.id.ET_hrsaida);
+        txt_saida = findViewById(R.id.txt_saida);
     }
+
+    @SuppressLint("SetTextI18n")
     public void horacalculo(View View){
         int entrada = Integer.parseInt(ET_hrentrada.getText().toString());
 
@@ -24,11 +28,11 @@ public class Activity_ex4 extends AppCompatActivity {
         int saida2 = (entrada + 6) - 24;
 
         if(entrada >= 18){
-            ET_hrsaida.setText(saida2);
+            txt_saida.setText(saida2 + "Horas");
 
         }
         else {
-            ET_hrsaida.setText(saida);
+            txt_saida.setText(saida + "Horas");
         }
 
 
