f79d29380535059cff89a73e9d26993478ad53d7
evelynkarina44 <evelyn.goncalves12@etec.sp.gov.br>
2023-08-23 21:47:03 -0300

    Arrumando a VideoView

--- app/src/main/java/com/example/app_exercicios/Activity_ex1.java
+++ app/src/main/java/com/example/app_exercicios/Activity_ex1.java
@@ -1,9 +1,12 @@
 package com.example.app_exercicios;
 
 import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,8 +16,11 @@ public class Activity_ex1 extends AppCompatActivity {
     private EditText ET_valorb;
     private EditText ET_resultado;
 
+        public Activity_ex1() {
+        }
 
-    /*"chamando" as Views pelo Id*/
+
+        /*"chamando" as Views pelo Id*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +28,26 @@ public class Activity_ex1 extends AppCompatActivity {
         ET_valora = findViewById(R.id.ET_valora);
         ET_valorb = findViewById(R.id.ET_valorb);
         ET_resultado = findViewById(R.id.ET_resultado);
+        VideoView video_view = findViewById(R.id.video_view);
+
+        // Definindo o caminho do vídeo a ser reproduzido (a partir da pasta 'res/raw')
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.multiplos;
+
+        // Configura a URI do vídeo
+        Uri uri = Uri.parse(videoPath);
+
+        // Colocando controladores no video
+        // Criando o objeto MediaController e instânciando ele
+        MediaController mediaController = new MediaController(this);
+
+        // Configurando o MediaContoller para usar no VideoView, colocando os controles abaixo do vídeo para o usuário controlá-lo
+        video_view.setMediaController(mediaController);
+
+        //Define que o VideoView é a âncora do MediaController, ou seja, o MediaController será exibido na parte de baixo do VideoView e vai seguir o comportamento do VideoView
+        mediaController.setAnchorView(video_view);
+
+        // Define a URI na VideoView
+        video_view.setVideoURI(uri);
 
     }
 
@@ -43,4 +69,5 @@ public class Activity_ex1 extends AppCompatActivity {
             ET_resultado.setText("São multiplos");
         }
     }
-}
+
+    }
